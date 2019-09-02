31977a6342434dc073eb45989c7eb6f78ba2245d
Gangadhard2 <gangadhar.d2009@gmail.com>
2019-09-02 08:10:25 -0700

    adding comments for home.js

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Home.css';
 import Header from '../../common/header/Header';
 import Card from '@material-ui/core/Card';
@@ -8,7 +8,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import IconButton from '@material-ui/core/IconButton';
 import Avatar from '@material-ui/core/Avatar';
-import {withStyles} from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import FavoriteIconBorder from '@material-ui/icons/FavoriteBorder';
 import FavoriteIconFill from '@material-ui/icons/Favorite';
 import Typography from '@material-ui/core/Typography';
@@ -18,9 +18,11 @@ import Input from '@material-ui/core/Input';
 import Button from '@material-ui/core/Button';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
-import {constants} from '../../common/utils'
+import { constants } from '../../common/utils'
 
-const styles =  theme => ({
+
+//CSS stsyling 
+const styles = theme => ({
   card: {
     maxWidth: 1100,
   },
@@ -28,37 +30,37 @@ const styles =  theme => ({
     margin: 10,
   },
   media: {
-    height:0,
+    height: 0,
     paddingTop: '56.25%', // 16:9
   },
   formControl: {
-    display:'flex',
-    flexDirection:'row',
-    justifyContent:'space-between',
-    alignItems:'baseline',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'baseline',
   },
-  comment:{
-    display:'flex',
-    alignItems:'center'
+  comment: {
+    display: 'flex',
+    alignItems: 'center'
   },
-  hr:{
-    marginTop:'10px',
-    borderTop:'2px solid #f2f2f2'
+  hr: {
+    marginTop: '10px',
+    borderTop: '2px solid #f2f2f2'
   },
-  gridList:{
+  gridList: {
     width: 1100,
     height: 'auto',
     overflowY: 'auto',
   },
-  grid:{
-    display:'flex',
-    justifyContent:'center',
-    alignItems:'center',
-    marginTop:90
+  grid: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 90
   }
 });
 
-class Home extends Component{
+class Home extends Component {
 
   constructor(props) {
     super(props);
@@ -67,29 +69,29 @@ class Home extends Component{
     }
     this.state = {
       data: [],
-      filteredData:[],
-      userData:{},
-      likeSet:new Set(),
-      comments:{},
-      currrentComment:""
+      filteredData: [],
+      userData: {},
+      likeSet: new Set(),
+      comments: {},
+      currrentComment: ""
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getUserInfo();
     this.getMediaData();
   }
 
-  render(){
-    const{classes} = this.props;
-    return(
+  render() {
+    const { classes } = this.props;
+    return (
       <div>
         <Header
           userProfileUrl={this.state.userData.profile_picture}
           screen={"Home"}
           searchHandler={this.onSearchEntered}
           handleLogout={this.logout}
-          handleAccount={this.navigateToAccount}/>
+          handleAccount={this.navigateToAccount} />
         <div className={classes.grid}>
           <GridList className={classes.gridList} cellHeight={'auto'}>
             {this.state.filteredData.map(item => (
@@ -100,7 +102,7 @@ class Home extends Component{
                   onLikedClicked={this.likeClickHandler}
                   onAddCommentClicked={this.addCommentClickHandler}
                   commentChangeHandler={this.commentChangeHandler}
-                  comments={this.state.comments}/>
+                  comments={this.state.comments} />
               </GridListTile>
             ))}
           </GridList>
@@ -109,7 +111,8 @@ class Home extends Component{
     );
   }
 
-  onSearchEntered = (value) =>{
+  //functinalaity for searching the hashtags
+  onSearchEntered = (value) => {
     let filteredData = this.state.data;
     filteredData = filteredData.filter((data) => {
       if (data.caption !== null && value !== "") {
@@ -126,7 +129,9 @@ class Home extends Component{
     })
   }
 
-  likeClickHandler = (id) =>{
+  //function for likes handling
+
+  likeClickHandler = (id) => {
     var foundItem = this.state.data.find((item) => {
       return item.id === id;
     })
@@ -134,101 +139,108 @@ class Home extends Component{
     if (typeof foundItem !== undefined) {
       if (!this.state.likeSet.has(id)) {
         foundItem.likes.count++;
-        this.setState(({likeSet}) => ({
-          likeSet:new Set(likeSet.add(id))
+        this.setState(({ likeSet }) => ({
+          likeSet: new Set(likeSet.add(id))
         }))
-      }else {
+      } else {
         foundItem.likes.count--;
-        this.setState(({likeSet}) =>{
+        this.setState(({ likeSet }) => {
           const newLike = new Set(likeSet);
           newLike.delete(id);
 
           return {
-            likeSet:newLike
+            likeSet: newLike
           };
         });
       }
     }
   }
 
-  addCommentClickHandler = (id)=>{
+  //function for comments 
+
+  addCommentClickHandler = (id) => {
     if (this.state.currentComment === "" || typeof this.state.currentComment === undefined) {
       return;
     }
 
-    let commentList = this.state.comments.hasOwnProperty(id)?
-      this.state.comments[id].concat(this.state.currentComment): [].concat(this.state.currentComment);
+    let commentList = this.state.comments.hasOwnProperty(id) ?
+      this.state.comments[id].concat(this.state.currentComment) : [].concat(this.state.currentComment);
 
     this.setState({
-      comments:{
+      comments: {
         ...this.state.comments,
-        [id]:commentList
+        [id]: commentList
       },
-      currentComment:''
+      currentComment: ''
     })
   }
 
 
   commentChangeHandler = (e) => {
     this.setState({
-      currentComment:e.target.value
+      currentComment: e.target.value
     });
   }
+
+  //getting the user info from sessionstaorage
 
   getUserInfo = () => {
     let that = this;
     let url = `${constants.userInfoUrl}/?access_token=${sessionStorage.getItem('access-token')}`;
-    return fetch(url,{
-      method:'GET',
-    }).then((response) =>{
-        return response.json();
-    }).then((jsonResponse) =>{
+    return fetch(url, {
+      method: 'GET',
+    }).then((response) => {
+      return response.json();
+    }).then((jsonResponse) => {
       that.setState({
-        userData:jsonResponse.data
+        userData: jsonResponse.data
       });
     }).catch((error) => {
-      console.log('error user data',error);
+      console.log('error user data', error);
     });
   }
 
+  //getting the jsosn data
   getMediaData = () => {
     let that = this;
     let url = `${constants.userMediaUrl}/?access_token=${sessionStorage.getItem('access-token')}`;
-    return fetch(url,{
-      method:'GET',
-    }).then((response) =>{
-        return response.json();
-    }).then((jsonResponse) =>{
+    return fetch(url, {
+      method: 'GET',
+    }).then((response) => {
+      return response.json();
+    }).then((jsonResponse) => {
       that.setState({
-        data:jsonResponse.data,
-        filteredData:jsonResponse.data
+        data: jsonResponse.data,
+        filteredData: jsonResponse.data
       });
     }).catch((error) => {
-      console.log('error user data',error);
+      console.log('error user data', error);
     });
   }
+
+  //function for logout
 
   logout = () => {
     sessionStorage.clear();
     this.props.history.replace('/');
   }
-
-  navigateToAccount = () =>{
+//function for navigation
+  navigateToAccount = () => {
     this.props.history.push('/profile');
   }
 }
 
-class HomeItem extends Component{
-  constructor(){
+class HomeItem extends Component {
+  constructor() {
     super();
     this.state = {
-      isLiked : false,
-      comment:'',
+      isLiked: false,
+      comment: '',
     }
   }
 
-  render(){
-    const {classes, item, comments} = this.props;
+  render() {
+    const { classes, item, comments } = this.props;
 
     let createdTime = new Date(0);
     createdTime.setUTCSeconds(item.created_time);
@@ -240,16 +252,16 @@ class HomeItem extends Component{
     let MM = createdTime.getMinutes();
     let ss = createdTime.getSeconds();
 
-    let time = dd+"/"+mm+"/"+yyyy+" "+HH+":"+MM+":"+ss;
-    let hashTags = item.tags.map(hash =>{
-      return "#"+hash;
+    let time = dd + "/" + mm + "/" + yyyy + " " + HH + ":" + MM + ":" + ss;
+    let hashTags = item.tags.map(hash => {
+      return "#" + hash;
     });
-    return(
+    return (
       <div className="home-main-container">
         <Card className={classes.card}>
           <CardHeader
             avatar={
-              <Avatar alt="User Profile Pic" src={item.user.profile_picture} className={classes.avatar}/>
+              <Avatar alt="User Profile Pic" src={item.user.profile_picture} className={classes.avatar} />
             }
             title={item.user.username}
             subheader={time}
@@ -259,32 +271,32 @@ class HomeItem extends Component{
               className={classes.media}
               image={item.images.standard_resolution.url}
             />
-            <div  className={classes.hr}>
+            <div className={classes.hr}>
               <Typography component="p">
-              {(item.caption !== null) && (item.caption.text).substring(0, item.caption.text.indexOf('#'))}
+                {(item.caption !== null) && (item.caption.text).substring(0, item.caption.text.indexOf('#'))}
               </Typography>
-              <Typography style={{color:'#4dabf5'}} component="p" >
+              <Typography style={{ color: '#4dabf5' }} component="p" >
                 {hashTags.join(' ')}
               </Typography>
             </div>
           </CardContent>
 
-            <CardActions>
-              <IconButton aria-label="Add to favorites" onClick={this.onLikeClicked.bind(this,item.id)}>
-                {this.state.isLiked && <FavoriteIconFill style={{color:'#F44336'}}/>}
-                {!this.state.isLiked && <FavoriteIconBorder/>}
-              </IconButton>
-              <Typography component="p">
-                {item.likes.count === 1 &&  <span>{item.likes.count} like</span>}
-                {item.likes.count !== 1 &&  <span>{item.likes.count} likes</span>}
-              </Typography>
-            </CardActions>
+          <CardActions>
+            <IconButton aria-label="Add to favorites" onClick={this.onLikeClicked.bind(this, item.id)}>
+              {this.state.isLiked && <FavoriteIconFill style={{ color: '#F44336' }} />}
+              {!this.state.isLiked && <FavoriteIconBorder />}
+            </IconButton>
+            <Typography component="p">
+              {item.likes.count === 1 && <span>{item.likes.count} like</span>}
+              {item.likes.count !== 1 && <span>{item.likes.count} likes</span>}
+            </Typography>
+          </CardActions>
 
-            <CardContent>
-            {comments.hasOwnProperty(item.id) && comments[item.id].map((comment, index)=>{
-              return(
+          <CardContent>
+            {comments.hasOwnProperty(item.id) && comments[item.id].map((comment, index) => {
+              return (
                 <div key={index} className="row">
-                  <Typography component="p" style={{fontWeight:'bold'}}>
+                  <Typography component="p" style={{ fontWeight: 'bold' }}>
                     <span>{item.user.username}:&nbsp;</span>
                   </Typography>
                   <Typography component="p" >
@@ -294,13 +306,13 @@ class HomeItem extends Component{
               )
             })}
             <div className={classes.formControl}>
-              <FormControl style={{flexGrow:1}}>
+              <FormControl style={{ flexGrow: 1 }}>
                 <InputLabel htmlFor="comment">Add Comment</InputLabel>
-                <Input id="comment" value={this.state.comment} onChange={this.commentChangeHandler}/>
+                <Input id="comment" value={this.state.comment} onChange={this.commentChangeHandler} />
               </FormControl>
               <FormControl>
-                <Button onClick={this.onAddCommentClicked.bind(this,item.id)}
-                   variant="contained" color="primary">
+                <Button onClick={this.onAddCommentClicked.bind(this, item.id)}
+                  variant="contained" color="primary">
                   ADD
                 </Button>
               </FormControl>
@@ -314,11 +326,11 @@ class HomeItem extends Component{
   onLikeClicked = (id) => {
     if (this.state.isLiked) {
       this.setState({
-        isLiked:false
+        isLiked: false
       });
-    }else {
+    } else {
       this.setState({
-        isLiked:true
+        isLiked: true
       });
     }
     this.props.onLikedClicked(id)
@@ -326,17 +338,17 @@ class HomeItem extends Component{
 
   commentChangeHandler = (e) => {
     this.setState({
-      comment:e.target.value,
+      comment: e.target.value,
     });
     this.props.commentChangeHandler(e);
   }
-
+//function for comments
   onAddCommentClicked = (id) => {
     if (this.state.comment === "" || typeof this.state.comment === undefined) {
       return;
     }
     this.setState({
-      comment:""
+      comment: ""
     });
     this.props.onAddCommentClicked(id);
   }
